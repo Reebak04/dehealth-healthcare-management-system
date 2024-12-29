756aefa0dc7f39a1b04c4dde41ed557e5cd3db83
Vanitha-SM <vanithas.student@saveetha.ac.in>
2024-12-29 10:26:54 +0530

    okay

--- contracts/main.sol
+++ contracts/main.sol
@@ -6,8 +6,7 @@ contract Upload {
   
   struct Access{
      address user; 
-     bool access;
-      //true or false
+     bool access; //true or false
   }
   mapping(address=>string[]) value;
   mapping(address=>mapping(address=>bool)) ownership;
